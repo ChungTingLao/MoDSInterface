0aba2a1f729c542630d00476d6ea24b4c13a80f7
ChungTingLao <chung.ting.lao.34@gmail.com>
2024-03-22 17:32:50 +0000

    dev-drive-cycle-success-story: regenerate input DLite with new mods ontology and with time in input.

--- d2c2d/dlitecudsbridge.py
+++ d2c2d/dlitecudsbridge.py
@@ -6,13 +6,14 @@ from simphony_osp.session import core_session
 from simphony_osp.tools import import_file, export_file
 from dlite_cuds.dlite2cuds import dlite2cuds
 from dlite_cuds.cuds2dlite import create_instance
-from simphony_osp.namespaces import mods
 
 work_dir = Path(__file__).parent.resolve()
 dlite.storage_path.append(work_dir/"entities")
 mappings = dlite.Collection.from_location("json",work_dir/"mappings"/"mappings_ss5.json",)
 
 def d2c(input_dlite,input_cuds_external):
+    
+    from simphony_osp.namespaces import mods
 
     core_session.clear(force=True)
 
@@ -23,13 +24,17 @@ def d2c(input_dlite,input_cuds_external):
 
     all_uuid = [subject for subject, predicate, obj in dlite_all.get_relations() if predicate == "_is-a"]
     
-    cuds_instances = [dlite2cuds(simphony_session=core_session,ontology=mods,instance=dlite_inst,
-                            mappings=mappings,mapping_iri="http://emmo.info/domain-mappings#mapsTo",relations=dlite_all,) 
-                      for dlite_inst in [dlite_all.get(uuid) for uuid in all_uuid]]
+    dict_d2c={}
+    
+    for uuid in all_uuid:
+        dlite_inst=dlite_all.get(uuid)
+        cuds_inst=dlite2cuds(simphony_session=core_session,ontology=mods,instance=dlite_inst,
+                             mappings=mappings,mapping_iri="http://emmo.info/domain-mappings#mapsTo",relations=dlite_all,)
+        dict_d2c[uuid]=cuds_inst
 
     export_file(core_session,file=str(work_dir/input_cuds_external))
 
-def c2d(output_cuds_internal,output_dlite):
+def c2d(output_cuds_internal,output_dlite,list_c=None):
         
     core_session.clear(force=True)
 
@@ -39,7 +44,8 @@ def c2d(output_cuds_internal,output_dlite):
 
     collection = dlite.Collection(id="uuid-collection")
 
-    for c in ['DataPointItem','DataPoint','OutputData']:
+    #for c in ['DataPointItem','DataPoint','OutputData']: #output?
+    for c in list_c:
 
         label, coll = create_instance(simphony_session=core_session,cuds_class_iri="http://www.osp-core.com/mods#"+c,
             entity_uri="http://onto-ns.com/meta/0.1/"+c,mappings=mappings,collection=collection,)
--- d2c2d/main.py
+++ d2c2d/main.py
@@ -6,7 +6,8 @@ import drivecycleinterface as dci
 import dlitecudsbridge as dcb
 import simphonybridge as sb
 
-load_dotenv()
+#load_dotenv()""
+os.environ["MODS_AGENT_BASE_URL"]="http://localhost:58085"
 cwd = Path(__file__).parent.resolve()
 
 # force re-install ontologies for SimPhoNy 4
@@ -16,6 +17,7 @@ if True:
     import simphony_osp.tools.pico as pico
 
     list_namespaces=[x.name for x in pico.namespaces()]
+    for ns in list_namespaces: print(ns)
 
     if "mods" in list_namespaces:
         pico.uninstall("mods")
@@ -33,6 +35,8 @@ dcb.d2c(input_dlite=cwd/"input_collection.json",input_cuds_external=cwd/"input_c
 
 sb.downgrade(cwd/"input_cuds_4.ttl",cwd/"input_cuds_3.8.ttl")
 
+quit()
+
 # MoDS wrapper takes input CUDS and call MoDS Agent, eventually return output as CUDS
 
 input_cuds_internal=import_cuds(os.path.join(cwd,"input_cuds_3.8.ttl"),format="ttl")
@@ -48,4 +52,4 @@ sb.upgrade(cwd/"output_cuds_3.8.ttl",cwd/"output_cuds_4.ttl")
 dcb.c2d(output_cuds_internal=cwd/"output_cuds_4.ttl",output_dlite=os.path.join(cwd,"output_collection.json"))
 
 for file in [cwd/"input_cuds_3.8.ttl",cwd/"input_cuds_4.ttl",cwd/"output_cuds_3.8.ttl",cwd/"output_cuds_4.ttl"]:
-    if True: os.remove(file) # remove intermediate files
+    if False: os.remove(file) # remove intermediate files
